f2efcf1d0fbda48f4ef9e19c58bcd00451177395
Caue Paiva <128509727+caue-paiva@users.noreply.github.com>
2025-06-21 19:22:48 -0300

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -1,7 +1,7 @@
 -- Criação de hash index na tabela Aluno para a coluna CidadeEnd
 CREATE INDEX index_aluno_cidade_hash ON Aluno USING HASH (CidadeEnd);
 
-EXPLAIN ANALYZE 
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE)  
 SELECT * FROM Aluno
 WHERE CidadeEnd = 'Cidade 789';
 
@@ -9,7 +9,7 @@ WHERE CidadeEnd = 'Cidade 789';
 -- Criação de árvore B index na tabela Turma para a coluna ValorTotalMatricula
 CREATE INDEX index_turma_valor_btree ON Turma USING BTREE (ValorTotalMatricula);
 
-EXPLAIN ANALYZE 
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE) 
 SELECT * FROM Turma
 WHERE ValorTotalMatricula > 40000;
 
@@ -17,8 +17,27 @@ WHERE ValorTotalMatricula > 40000;
 -- Criação de árvore B index na tabela Mensagem para a coluna Timestamp
 CREATE INDEX index_mensagem_timestamp_btree ON Mensagem USING BTREE (Timestamp);
 
-EXPLAIN ANALYZE 
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE) 
 SELECT id, Timestamp::date as date, Texto, tipo
 FROM Mensagem
 WHERE Timestamp >= NOW() + INTERVAL '100 days'
   AND Timestamp <= NOW() + INTERVAL '2200 days'
+
+-- Buscas para o tamanho das Tabelas e de seus Índices
+SELECT 
+    'turma' AS table_name,
+    pg_size_pretty(pg_relation_size('turma')) AS tamanho_so_tabela,
+    pg_size_pretty(pg_total_relation_size('turma')) AS tamanho_tabela_e_indice,
+    pg_size_pretty(pg_indexes_size('turma')) AS tamanho_indice;
+
+SELECT 
+    'Mensagem' AS table_name,
+    pg_size_pretty(pg_relation_size('Mensagem')) AS tamanho_so_tabela,
+    pg_size_pretty(pg_total_relation_size('Mensagem')) AS tamanho_tabela_e_indice,
+    pg_size_pretty(pg_indexes_size('Mensagem')) AS tamanho_indice;
+
+SELECT 
+    'Aluno' AS table_name,
+    pg_size_pretty(pg_relation_size('Aluno')) AS tamanho_so_tabela,
+    pg_size_pretty(pg_total_relation_size('Aluno')) AS tamanho_tabela_e_indice,
+    pg_size_pretty(pg_indexes_size('Aluno')) AS tamanho_indice;
